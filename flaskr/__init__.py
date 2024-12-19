529a82508286795ae8191a77b04164b26ea0fa08
Neo.Chang <neo.chang@qisda.com>
2024-12-19 14:09:49 +0800

    03-blueprint-view

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,8 +4,14 @@ from flask import Flask
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
+    # ---------
+    # debug message
     print(f"__name__: {__name__}")
     print(f"instance path: {app.instance_path}")
+    print(f"root path: {app.root_path}")
+    # ---------
+    
     app.config.from_mapping(
         SECRET_KEY="dev",
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
